e3dbe7ace5366974f638b62aebd5c5d0e3b5eb72
Guilherme Amorim Gonçalez <guilherme.goncalez@w12.com.br>
2023-10-18 18:47:36 -0300

    feat: add sqlite

---
+++ config/sqlite.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"os"
+
+	"github.com/gg-amorim/opportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating ...")
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			logger.Errorf("method: os.MkdirAll  [ERROR]: %v", err)
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			logger.Errorf("method: os.Create  [ERROR] %v", err)
+			return nil, err
+		}
+		file.Close()
+
+	}
+	db, err := gorm.Open(sqlite.Open("./db/main.db"), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
