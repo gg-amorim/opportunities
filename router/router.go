fbeaa57d70ceff9b0465ae98928ca4ee0df1e620
Guilherme Amorim Gonçalez <guilherme.goncalez@w12.com.br>
2023-10-18 15:29:40 -0300

    feat: add routes opening

--- router/router.go
+++ router/router.go
@@ -4,10 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	initializeRoutes(r)
+
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
