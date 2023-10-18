c2ca1792962d98e092e340494c22ab9eb028be90
Guilherme Amorim Gonçalez <guilherme.goncalez@w12.com.br>
2023-10-18 17:17:06 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gg-amorim/opportunities/router"
+import (
+	"github.com/gg-amorim/opportunities/config"
+	"github.com/gg-amorim/opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Init Congfigs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initializion error: %v", err)
+		return
+	}
+	// Init Router
 	router.Initialize()
 }
